4628c0310a91a8858168487ba3ba968ef1b6b395
José Buss <josecarlosbuss@gmail.com>
2019-10-14 22:55:29 -0300

    .
    .

---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,23 @@
+import { CursosService } from './cursos.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
+
+
+@NgModule({
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent],
+  providers: [
+    CursosService
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CursosComponent
+  ]
+})
+export class CursosModule { }
